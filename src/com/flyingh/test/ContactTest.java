f15e62c785d27db2a92556148c4db6f83a76f2ce
flycoding <flycoding@yeah.net>
2013-01-26 10:56:04 +0800

    test query data.

--- src/com/flyingh/test/ContactTest.java
+++ src/com/flyingh/test/ContactTest.java
@@ -7,6 +7,8 @@ import android.util.Log;
 
 public class ContactTest extends AndroidTestCase {
 	private static final String TAG = "ContactTest";
+	
+	
 
 	public void testQuery() {
 		Cursor cursor = getContext().getContentResolver().query(
@@ -17,6 +19,29 @@ public class ContactTest extends AndroidTestCase {
 			String display_name = cursor.getString(cursor
 					.getColumnIndex("display_name"));
 			Log.i(TAG, id + "---" + display_name);
+			Cursor dataCursor = getContext().getContentResolver().query(
+					Uri.parse("content://com.android.contacts/contacts/" + id
+							+ "/data"), new String[] { "data1", "mimetype" },
+					"raw_contact_id=?", new String[] { String.valueOf(id) },
+					null);
+			StringBuilder sb = new StringBuilder();
+			while (dataCursor.moveToNext()) {
+				String data1 = dataCursor.getString(dataCursor
+						.getColumnIndex("data1"));
+				String mimetype = dataCursor.getString(dataCursor
+						.getColumnIndex("mimetype"));
+				if ("vnd.android.cursor.item/phone_v2".equals(mimetype)) {
+					sb.append("phone:" + data1).append("-");
+				} else if ("vnd.android.cursor.item/postal-address_v2"
+						.equals(mimetype)) {
+					sb.append("address:" + data1).append("-");
+				} else if ("vnd.android.cursor.item/email_v2".equals(mimetype)) {
+					sb.append("email:" + data1).append("-");
+				} else if ("vnd.android.cursor.item/name".equals(mimetype)) {
+					sb.append("name:" + data1).append("-");
+				}
+			}
+			Log.i(TAG, sb.deleteCharAt(sb.length() - 1).toString());
 		}
 	}
 }
